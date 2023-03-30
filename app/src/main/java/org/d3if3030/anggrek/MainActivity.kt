0bedd37a35987107fe6a2a46197a46a4d9fc9b66
Safina Sinandari Pratiwi <safinapratiwi.sp@gmail.com>
2023-03-30 14:08:48 +0700

    Perbaikan Tata Letak Layout Button

--- app/src/main/java/org/d3if3030/anggrek/MainActivity.kt
+++ app/src/main/java/org/d3if3030/anggrek/MainActivity.kt
@@ -1,11 +1,21 @@
 package org.d3if3030.anggrek
 
 import androidx.appcompat.app.AppCompatActivity
+import android.util.Log
+import android.widget.Button
 import android.os.Bundle
+import org.d3if3030.anggrek.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.cariButton.setOnClickListener { cariAnggrek() }
     }
+    private fun cariAnggrek() {
+        Log.d("MainActivity", "Tombol diklik!")
+    }
+
 }
